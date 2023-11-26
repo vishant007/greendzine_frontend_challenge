68b6b5c6f1b42a4400e28d08478fc32bec72e2ff
vishant007 <vishant9797@gmail.com>
2023-11-26 22:07:46 +0530

    fix: dashboard-ui

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
+import logo from '../Assets/moptro.png';
+import UserLogo from '../Assets/User.png';
 
 const Dashboard = () => {
-	// Assuming you have data for each day's progress (in percentage)
+	const navigate = useNavigate();
+
 	const progressData = {
 		Monday: 4,
 		Tuesday: 60,
@@ -12,19 +16,32 @@ const Dashboard = () => {
 		Saturday: 50,
 	};
 
+	const redirectToUserList = () => {
+		navigate('/user-list');
+	};
+
 	return (
-		<div className='dashboard'>
-			<h2>Employee Productivity Dashboard:</h2>
-			{Object.entries(progressData).map(([day, progress]) => (
-				<div key={day} className='progress-bar-container'>
-					<h4>
-						Productivity on {day}: {progress}%
-					</h4>
-					<div className='progress-bar'>
-						<div className='progress' style={{ width: `${progress}%` }}></div>
+		<div className='main-dashboard'>
+			<div className='logo-container' onClick={redirectToUserList}>
+				<img src={UserLogo} alt='user-logo' />
+			</div>
+			<div className='dashboard-header'>
+				<img src={logo} alt='logo'></img>
+			</div>
+			<div className='dashboard'>
+				<h3>Employee Productivity Dashboard:</h3>
+				{Object.entries(progressData).map(([day, progress]) => (
+					<div key={day} className='progress-bar-container'>
+						<div className='day'>
+							<h4>Productivity on {day}:</h4>
+							<h5>{progress}%</h5>
+						</div>
+						<div className='progress-bar'>
+							<div className='progress' style={{ width: `${progress}%` }}></div>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 };
